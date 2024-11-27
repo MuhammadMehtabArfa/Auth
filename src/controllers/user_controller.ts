4977f807ecde884df04387218aad83a0ec961a55
iusmankh1 <alina.saqib500@gmail.com>
2024-11-27 19:44:16 +0500

    solved issues and updated code

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -1,71 +1,64 @@
 import { Request, response, Response } from "express";
-import UserModel,{IUser} from "../model/UserModel";
+import UserModel, { IUser } from "../model/UserModel";
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken"
-
-
+import jwt from "jsonwebtoken";
+import { generateToken } from "../utility/generateToken";
 
 // Signup
-export const signup = async (req: Request, res: Response): Promise<Response> => {
+export const signup = async (req: Request, res: Response): Promise<void> => {
   try {
-    
-const { username, email, password } = req.body
+    const { username, email, password } = req.body;
 
-  const existing_user = await UserModel.findOne({ email });
-
-  if(existing_user){
-    return res.status(400).json({message: "USER ALREADY EXISTS"})
-  }
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser : IUser= new UserModel({
-    username,
-    email,
-    password: hashedPassword,
-  })
+    // Hash password and create new user
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser: IUser = new UserModel({
+      username,
+      email,
+      password: hashedPassword,
+    });
     await newUser.save();
 
-  return res.status(201).json({ message: "User created successfully" });
-} catch (error) {
-  return res.status(500).json({ message: "Server error", error: error.message });
-}
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error: any) {
+    console.log(error);
+    if (error?.code === 11000) {
+      const field = error?.message.includes("email") ? "email" : "username";
+      res.status(401).json({
+        message: `${
+          field.charAt(0).toUpperCase() + field.slice(1)
+        } must be unique`,
+      });
+      return;
+    }
+
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
-
-
-
 // Login
-export const login = async (req: Request, res: Response): Promise<Response> => {
-try {
-    const {email, password}= req.body
-    const user = await UserModel.findOne({email})
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const user: IUser | null = await UserModel.findOne({ email });
 
-    if (!user){
-        return res.status(404).json({ message: "User not found" });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-    
+
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
-  
-   const token = jwt.sign(
-    { id: user._id, email: user.email },
-    JWT_SECRET,
-    { expiresIn: "1h" } 
-  );
 
-  return res.status(200).json({ message: "Login successful", token });
-} catch (error) {
-  return res.status(500).json({ message: "Server error", error.message });
-}
+    const access_token = generateToken(user?._id, user?.email);
+
+    return res.status(200).json({ message: "Login successful", access_token });
+  } catch (error: any) {
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error?.message });
+  }
 };
 
-
-
-
-
 // Email Verification
-
-
-
