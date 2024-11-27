5ef5b342b7857b9557ccab7cc1d47c0f5f573ab1
MUHAMMADMEHTABARFA <muhammadmehtabarfa@gmail.com>
2024-11-27 19:48:44 +0500

    hey this is my first auth project

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -1,64 +1,79 @@
 import { Request, response, Response } from "express";
-import UserModel,{IUser} from "../model/UserModel";
+import UserModel, { IUser } from "../model/UserModel";
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken"
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET || "default_secret_key";
 
 
 // Signup
-export const signup = async (req: Request, res: Response): Promise<Response> => {
-  try {
-    
-const { username, email, password } = req.body
+export const signup = async (req: Request, res: Response): Promise<void> => {
+    try {
 
-  const existing_user = await UserModel.findOne({ email });
+        const { username, email, password } = req.body
 
-  if(existing_user){
-    return res.status(400).json({message: "USER ALREADY EXISTS"})
-  }
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser : IUser= new UserModel({
-    username,
-    email,
-    password: hashedPassword,
-  })
-    await newUser.save();
+        const existing_user = await UserModel.findOne({ email });
 
-  return res.status(201).json({ message: "User created successfully" });
-} catch (error) {
-  return res.status(500).json({ message: "Server error", error: error.message });
-}
+        if (existing_user) {
+            res.status(400).json({ message: "USER ALREADY EXISTS" });
+            return;
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser: IUser = new UserModel({
+            username,
+            email,
+            password: hashedPassword,
+        })
+        await newUser.save();
+
+        res.status(201).json({ message: "User created successfully" });
+        return
+    } catch (error: any) {
+
+        res.status(500).json({ message: "Server error", error: error.message });
+        return
+    }
 };
 
 
 
 
 // Login
-export const login = async (req: Request, res: Response): Promise<Response> => {
-try {
-    const {email, password}= req.body
-    const user = await UserModel.findOne({email})
+export const login = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { email, password } = req.body
+        const user = await UserModel.findOne({ email })
 
-    if (!user){
-        return res.status(404).json({ message: "User not found" });
-    }
-    
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
-  
-   const token = jwt.sign(
-    { id: user._id, email: user.email },
-    JWT_SECRET,
-    { expiresIn: "1h" } 
-  );
+        if (!user) {
+            res.status(404).json({ message: "User not found" });
+            return;
+        }
 
-  return res.status(200).json({ message: "Login successful", token });
-} catch (error) {
-  return res.status(500).json({ message: "Server error", error.message });
-}
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            res.status(401).json({ message: "Invalid credentials" });
+            return
+        }
+
+        const token = jwt.sign(
+
+
+            { id: user._id, email: user.email },
+            JWT_SECRET,
+            { expiresIn: "1h" }
+        );
+
+        res.status(200).json({ message: "Login successful", token });
+        return
+    } catch (error) {
+        const err = error as any
+        res.status(500).json({ message: "Server error", error: err.message });
+        return
+    }
 };
 
 
--- src/routes/user_routes.ts
+++ src/routes/user_routes.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { signup, login} from "../controllers/user_controller"; // Adjust the path as needed
+import { signup, login } from "../controllers/user_controller"; // Adjust the path as needed
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+router.post("/signup", signup as any);
+router.post("/login", login as any);
 
 export default router;
